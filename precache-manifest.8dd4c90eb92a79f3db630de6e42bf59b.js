770e98417fccf0e9a350e85008aed29e12b6e991
root <root@DESKTOP-35OHEOS.localdomain>
2020-08-11 22:06:01 +0900

    Updates

---
+++ precache-manifest.8dd4c90eb92a79f3db630de6e42bf59b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28638821ccca6daf7e2ac30935cc41fe",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a8b934455800c4d187c3",
+    "url": "/movie_app/static/css/main.bcb141d1.chunk.css"
+  },
+  {
+    "revision": "b99ceaa4695b879ef91e",
+    "url": "/movie_app/static/js/2.0ddf303e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.0ddf303e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b934455800c4d187c3",
+    "url": "/movie_app/static/js/main.15455113.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
