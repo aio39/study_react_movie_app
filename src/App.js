5e8b89c7aabedbff8bbaea45c934473161ff7c0c
root <root@DESKTOP-35OHEOS.localdomain>
2020-08-11 21:51:48 +0900

    4.5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { render } from "react-dom";
-
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false }); // movies:movies 와 같음
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 2000);
+    this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"} </div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader_text">Loading... </span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.key}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
